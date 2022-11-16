f3b59880f776edf21d2537fc9a476a26674b52e5
MichaelDHuy <dachuy87@gmail.com>
2022-11-15 20:33:24 -0800

    Two-O-Player-Math-Game

---
+++ game.rb
@@ -0,0 +1,34 @@
+class Game
+
+  def initialize
+    @player1 = Player.new('Player 1')
+    @player2 = Player.new('Player 2')
+    @player_turn = @player1
+  end
+
+  def play_game
+    #loop through until someone loses.
+    while @player_turn.lives > 0 do
+      #ask a question to a player
+      "#{Question.new.ask_question(@player_turn)}"
+      puts "P1: #{@player1.lives}/3 VS P2: #{@player2.lives}/3"
+      #check if game ends
+      if @player_turn.lives == 0
+        puts "#{@player_turn.name} loses the game!"
+        return
+      end
+
+
+      #change who's turn it is
+      if @player_turn.name == @player1.name
+        @player_turn = @player2
+      else
+        @player_turn = @player1
+      end
+    
+    puts "----- NEW TURN -----"
+  
+    end
+  end
+
+end 
---
+++ question.rb
@@ -0,0 +1,29 @@
+require './player.rb'
+class Question
+
+ attr_reader :num1, :num2, :answer
+ 
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @answer = num1 + num2
+  end
+
+  def ask_question(player)
+    puts " #{player.name}: What is #{num1} + #{num2} ?"
+    reply = gets.chomp.to_i
+    if reply !=@answer
+      incorrect(player)
+      puts 'Incorrect!'
+    else
+    puts 'Correct!'
+    end
+
+  end
+
+  def incorrect(player)
+    player.lose_life
+  end
+
+
+end
